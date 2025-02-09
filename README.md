3247a37ed55394992f3f8c11a3bcc26a9143fd8f
plainionist <mail@plainionist.net>
2025-02-09 16:52:57 +0100

    markdown analysis with "rust_bert"

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
 
 Using the power of graphs and 3D rendering to structure ideas
 
+# Building
+
+If downloading libtorch does not work, download manually and set LIBTORCH to the extracted folder
+
+# Run
+
+set
+
+```powershell
+$env:PATH += ";C:\path\to\libtorch\lib"
+```
+
 # References
 
 - https://github.com/vasturiano/3d-force-graph
--- src-tauri/src/markdown_analysis.rs
+++ src-tauri/src/markdown_analysis.rs
@@ -1,9 +1,15 @@
+use rust_bert::pipelines::pos_tagging::{POSConfig, POSModel, POSTag};
 use std::collections::HashMap;
+use std::collections::HashSet;
 use std::fs;
-use std::io::{self, Read};
+use std::io;
 use std::path::Path;
+use std::sync::LazyLock;
 
-fn scan_markdown_files(dir: &Path) -> io::Result<HashMap<String, String>> {
+static NOUN_TAGS: LazyLock<HashSet<&'static str>> =
+    LazyLock::new(|| ["NN", "NNS", "NNP", "NNPS"].iter().cloned().collect());
+
+fn scan_markdown_files(dir: &Path, model: &POSModel) -> io::Result<HashMap<String, Vec<String>>> {
     let mut file_word_map = HashMap::new();
 
     if dir.is_dir() {
@@ -11,16 +17,30 @@ fn scan_markdown_files(dir: &Path) -> io::Result<HashMap<String, String>> {
             if file_word_map.len() >= 50 {
                 break;
             }
+
             let entry = entry?;
             let path = entry.path();
 
             if path.is_dir() {
-                let nested_files = scan_markdown_files(&path)?;
+                let nested_files = scan_markdown_files(&path, model)?;
                 file_word_map.extend(nested_files);
             } else if path.extension().map_or(false, |ext| ext == "md") {
                 if let Ok(contents) = fs::read_to_string(&path) {
-                    let words: Vec<&str> = contents.split_whitespace().collect();
-                    file_word_map.insert(path.to_string_lossy().to_string(), words.join(" "));
+                    let predictions = model.predict(&[contents]);
+
+                    let proper_nouns: Vec<String> = predictions
+                        .into_iter()
+                        .flatten()
+                        .filter_map(|token: POSTag| {
+                            if NOUN_TAGS.contains(token.label.as_str()) {
+                                Some(token.word)
+                            } else {
+                                None
+                            }
+                        })
+                        .collect();
+
+                    file_word_map.insert(path.to_string_lossy().to_string(), proper_nouns);
                 }
             }
         }
@@ -29,6 +49,9 @@ fn scan_markdown_files(dir: &Path) -> io::Result<HashMap<String, String>> {
     Ok(file_word_map)
 }
 
-pub fn analyze(dir: &Path) -> std::io::Result<String> {
-    scan_markdown_files(dir).map(|_| "{}".to_string())
+pub fn analyze(dir: &Path) -> io::Result<String> {
+    let config = POSConfig::default();
+    let model = POSModel::new(config).map_err(|e| io::Error::new(io::ErrorKind::Other, e.to_string()))?;
+
+    scan_markdown_files(dir, &model).map(|result| serde_json::to_string(&result).unwrap())
 }
