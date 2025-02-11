0435b27a9ecb5c0539c83af60b1b3e22d256a5da
plainionist <mail@plainionist.net>
2025-02-11 18:44:31 +0100

    analyze csharp usings: file options added

--- experiments/analyze-csharp-usings.fsx
+++ experiments/analyze-csharp-usings.fsx
@@ -1,3 +1,4 @@
+open System
 open System.IO
 open System.Text.RegularExpressions
 open System.Text.Json
@@ -20,48 +21,86 @@ let extractUsingsAndNamespace (filePath: string) =
         |> Seq.tryPick(fun line ->
             let m = namespacePattern.Match(line)
             if m.Success then Some(m.Groups.[1].Value) else None)
+        |> Option.defaultValue "::global::"
     
     namespaceDecl, usings
 
-let extractGroup (namespaceName: string) =
+let extractGroupFromNamespace (namespaceName: string) =
     let parts = namespaceName.Split('.')
     if parts.Length > 1 then parts.[1] else "Default"
 
-let buildDependencyGraph (folder: string) =
-    let files = Directory.GetFiles(folder, "*.cs", SearchOption.AllDirectories)
+let extractGroupFromFile (root:string) (file: string) =
+    let parts = file.Substring(root.Length).Trim(Path.DirectorySeparatorChar).Split(Path.DirectorySeparatorChar)
+    if parts.Length > 1 then parts.[1] else "Default"
 
+let enumerateSourceFiles (folder:string) =
+    let sep = Path.DirectorySeparatorChar
+
+    Directory.EnumerateFiles(folder, "*.cs", SearchOption.AllDirectories)
+    |> Seq.filter (fun file -> file.EndsWith(".Designer.cs") |> not)
+    |> Seq.filter (fun file -> file.Contains($"{sep}obj{sep}", StringComparison.OrdinalIgnoreCase) |> not)
+    |> Seq.filter (fun file -> file.Contains($"{sep}bin{sep}", StringComparison.OrdinalIgnoreCase) |> not)
+
+let buildDependencyGraph (folder: string) =
     let namespaceUsings =
-        files
+        folder
+        |> enumerateSourceFiles 
         |> Seq.map extractUsingsAndNamespace
-        |> Seq.choose(fun (ns, usings) -> ns |> Option.map(fun n -> n, usings))
         |> Map.ofSeq
 
     let declaredNamespaces = namespaceUsings |> Map.keys |> Set.ofSeq
-    let filteredDependencies = namespaceUsings |> Map.map(fun _ usings -> Set.intersect usings declaredNamespaces)
+    let filteredDependencies = namespaceUsings |> Map.map(fun _ usings -> Set.intersect usings declaredNamespaces) |> Map.toList
 
     declaredNamespaces, filteredDependencies
 
-let generateJson (declaredNamespaces: Set<string>) (dependencies: Map<string, Set<string>>) =
-    let nodes =
-        declaredNamespaces
-        |> Seq.map(fun ns -> {| id = ns; group = extractGroup ns |})
+let buildFileDependencyGraph (folder: string) =
+    let fileUsings =
+        folder
+        |> enumerateSourceFiles 
+        |> Seq.map (fun file -> file, extractUsingsAndNamespace file)
+        |> Seq.map (fun (file, (ns, usings)) -> file, ns, usings)
         |> Seq.toList
+
+    let files = fileUsings |> List.map (fun (file, _, _) -> file) |> Set.ofList
+    let declaredNamespaces = fileUsings |> List.map (fun (_, ns, _) -> ns) |> Set.ofList
+    let filteredDependencies = fileUsings |> Seq.map (fun (file,_,usings) -> file, Set.intersect usings declaredNamespaces) |> List.ofSeq
+
+    files, filteredDependencies
+
+let generateJson extractGroup (nodes: Set<string>) (edges: (string * string) seq) =
+    let nodes =
+          nodes
+          |> Seq.map (fun ns -> {| id = ns; group = extractGroup ns |})
+          |> Seq.toList
     
     let links =
-        dependencies
-        |> Seq.collect(fun x -> x.Value |> Seq.map (fun target -> {| source = x.Key; target = target |}))
+        edges
+        |> Seq.map(fun (source, target) -> {| source = source; target = target |})
         |> Seq.toList
     
     JsonSerializer.Serialize({| nodes = nodes; links = links |}, JsonSerializerOptions(WriteIndented = true))
 
-if fsi.CommandLineArgs.Length < 1 then
+match fsi.CommandLineArgs with
+| [| _; folderPath |] ->
+    let declaredNamespaces, dependencies = buildDependencyGraph folderPath
+    let dependencies = dependencies |> Seq.collect(fun (ns, usings) -> usings |> Seq.map(fun dep -> ns, dep))
+
+    let jsonOutput = generateJson extractGroupFromNamespace declaredNamespaces dependencies
+    let outputFile = Path.Combine(folderPath, "namespace-deps.json")
+    File.WriteAllText(outputFile, jsonOutput)
+    
+    printfn "Dependency graph written to: %s" outputFile
+    0
+| [| _; folderPath; "-files" |] ->
+    let files, dependencies = buildFileDependencyGraph folderPath
+    let nodes = dependencies |> Seq.collect snd |> Seq.append files |> Set.ofSeq
+    let dependencies = dependencies |> Seq.collect(fun (file, usings) -> usings |> Seq.map(fun dep -> file, dep))
+
+    let jsonOutput = generateJson (extractGroupFromFile folderPath) nodes dependencies
+    let outputFile = Path.Combine(folderPath, "file-deps.json")
+    File.WriteAllText(outputFile, jsonOutput)
+    
+    printfn "File dependency graph written to: %s" outputFile
+    0
+| _ ->
     failwith "Usage: dotnet fsi analyze-csharp-usings.fsx <folder-path>"
-
-let workspace = fsi.CommandLineArgs[1]
-
-let declaredNamespaces, dependencies = buildDependencyGraph workspace
-let json = generateJson declaredNamespaces dependencies
-
-let outputFile = Path.Combine(workspace, "csharp-usings.json")
-File.WriteAllText(outputFile, json)
-printfn "Dependency graph written to: %s" outputFile
